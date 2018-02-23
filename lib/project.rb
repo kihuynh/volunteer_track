87b5e031cb1a82eeeccdb9ffb1ad97300cc4c69f
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-02-23 13:45:33 -0800

    pass all and save spec for projects.

--- lib/project.rb
+++ lib/project.rb
@@ -3,20 +3,29 @@ class Project
 
   def initialize(attributes)
     @title = attributes[:title]
-    # @id = attributes[:id]
-  end
-
-  def save
-    result = DB.exec("INSERT INTO projects (title) VALUES ('{#{@title}}') RETURNING id;")
-    @id = result.first().fetch("id").to_i()
-  end
-
-  def ==(extra_project)
-    self.title().==(extra_project.title())
+    @id = attributes[:id]
   end
 
   def self.all
     returned_projects = DB.exec("SELECT * FROM projects;")
     projects = []
+    returned_projects.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
   end
+
+  def save
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  def ==(extra_project)
+    self.title().==(extra_project.title()).&(self.id().==(extra_project.id()))
+  end
+
+
+
 end
