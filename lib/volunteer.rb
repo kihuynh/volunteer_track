031fb0e78c88291b5de93b6dadbbd510d2647231
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-02-23 09:26:24 -0800

    initalize files

---
+++ lib/volunteer.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby
+
+class Volunteer
+
+end
+
---
+++ spec/volunteer_spec.rb
@@ -0,0 +1,9 @@
+require('rspec')
+require('volunteer')
+require('pry')
+
+describe('volunteer') do
+  it("create new project") do
+  expect().to(eq())
+   end
+end
