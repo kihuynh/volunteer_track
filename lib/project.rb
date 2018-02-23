6575082680daf487dfb0f0ae85e3e8ad728300e3
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-02-23 13:05:26 -0800

    pass third spec of saving after project

--- lib/project.rb
+++ lib/project.rb
@@ -6,5 +6,12 @@ class Project
     # @id = attributes[:id]
   end
 
+  def save
+    result = DB.exec("INSERT INTO projects (title) VALUES ('{#{@title}}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
 
+  # def ==(extra_project)
+  #   self.name().==(extra_project.name())
+  # end
 end
